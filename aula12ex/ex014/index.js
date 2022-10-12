72d820314ef375befd7dd9f6f4a01b4880c363f0
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-10-12 16:01:27 -0300

    Update

---
+++ aula12ex/ex014/index.js
@@ -0,0 +1,24 @@
+function carregar() {
+    var msg = document.getElementById('msg');
+    var img = document.getElementById('img');
+    var data = new Date();
+    var horas = data.getHours();
+    var min = data.getMinutes();
+    var horas = 14
+    msg.innerHTML = `Agora são ${horas}:${min} horas`
+
+    if (horas >= 00 && horas < 12) {
+        // Bom dia!
+        document.body.style.background = '#026873'
+        img.scr = 'manha.400px.png'
+    } else if (horas >= 12 && horas <= 18) {
+        //Boa tarde!
+        document.body.style.background = '#595736'
+        img.src = 'tarde.400px.png'
+    } else {
+        //Boa noite
+        document.body.style.background = '#295073'
+        img.src = 'noite.400px.png'
+    }
+
+}
