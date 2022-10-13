190a101ad6d2e76485e886771beca4c1dbea17ba
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-10-13 15:01:30 -0300

    Update

---
+++ aula12ex/ex015/index.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert(' [ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ``
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked)  {
+            gênero = 'Homem'
+            if (idade >=0  && idade < 10) {
+                // criança
+                img.setAttribute('src', 'crianca.m.png')
+            } else if (idade >= 10 && idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem.m.png')
+            } else if (idade >= 21 && idade < 59 ) {
+                // Adulto
+                img.setAttribute('src', 'adulto.m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src','idoso.m.png')
+            }
+
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher' 
+            if (idade >=0  && idade < 10) {
+                // criança
+                img.setAttribute('src', 'crianca.f.png')
+            } else if (idade >= 10 && idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem.f.png')
+            } else if (idade >= 21 && idade < 59 ) {
+                // Adulto
+                img.setAttribute('src', 'adulto.f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosa.f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos .`
+        res.appendChild(img)
+    }
+
+}
