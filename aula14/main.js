194bd817f12757ceb4c01eda6ee3cf5d98975951
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-10-14 14:12:15 -0300

    Create main.js

---
+++ aula14/main.js
@@ -0,0 +1,28 @@
+
+
+//  Repetição com For
+console.log('Vai começar...');
+for (var c = 1; c <= 94; c++) {
+    console.log('c');
+}
+
+
+
+
+
+/* Repetição com While
+var c = 1 
+while(c <= 3) {
+    console.log('tb');
+    c++
+} 
+*/
+
+
+/* Repetição com Do While
+var c = 1 
+do {
+    console.log('tb');
+    c++
+} while(c <= 3)
+*/
